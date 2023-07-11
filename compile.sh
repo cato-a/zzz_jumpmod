3d750437576094b2d279a9504a9b3693c1780274
Cato <121131666+cato-a@users.noreply.github.com>
2023-07-11 17:02:50 +0200

    Upgraded compile.sh a bit

--- compile.sh
+++ compile.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
-zip -r zzz_jumpmod.pk3 callback.gsc maps jumpmod -x jumpmod/settings.gsc
+
+JUMPMOD_NAME='zzz_jumpmod'
+JUMPMOD_PATH="$(pwd)"
+JUMPMOD_FILES=(callback.gsc maps jumpmod)
+
+if [[ -d "$JUMPMOD_PATH" && "$(basename "$JUMPMOD_PATH")" == "$JUMPMOD_NAME" ]]; then
+    if [[ ("$1" == 'j2g' || "$1" == 'json2gsc') && -f "$JUMPMOD_PATH/json2gsc/Build/Staged/json2gsc" ]]; then
+        "$JUMPMOD_PATH/json2gsc/Build/Staged/json2gsc" "$JUMPMOD_PATH/json2gsc/Build/Staged/mapconfigs" "$JUMPMOD_PATH/jumpmod/settings.gsc"
+    fi
+
+    zip -r "$JUMPMOD_NAME.pk3" "${JUMPMOD_FILES[@]}"
+fi
